7da9a5d3e1333b59c58b2384a2ff77238cae14b0
Aadit Gupta <spacegiant21@gmail.com>
2020-09-19 11:52:28 -0700

    start tracking

--- backend_endpoint/api/parts/tracking/index.ts
+++ backend_endpoint/api/parts/tracking/index.ts
@@ -1,11 +1,32 @@
-import { NowRequest, NowResponse } from '@vercel/node'
+import { NowRequest, NowResponse } from "@vercel/node";
 
-
+const tracker = require("delivery-tracker");
 
 module.exports = async (req: NowRequest, res: NowResponse) => {
-  const { tracking_id } = req.body
+  if (
+    req.body === undefined ||
+    req.body.tracking_id === undefined ||
+    req.body.courier_name === undefined
+  ) {
+    console.log(req.body);
+    res.send(
+      JSON.stringify({
+        err: "Invalid request: please specify tracking_id AND courier_name!",
+      })
+    );
+    return;
+  }
 
-  //get tracking
+  const { tracking_id, courier_name } = req.body;
+  const courier = tracker.courier(
+    tracker.COURIER[`${courier_name}`.toUpperCase()].CODE
+  );
 
-  res.send('result')
-}
+  let result;
+  courier.trace(tracking_id, function (err, res) {
+    result = res;
+  });
+
+  console.log(result);
+  res.send(result);
+};
