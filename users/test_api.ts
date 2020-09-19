ce5d9b33d18c7347e5e39b431145830593cb580d
rjawesome <aonedtop@gmail.com>
2020-09-19 10:19:00 -0700

    add authorization

---
+++ users/test_api.ts
@@ -0,0 +1,36 @@
+import firebase from '@firebase/app';
+import '@firebase/auth';
+import fetch from 'node-fetch';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBywkBF8HlaLDTgvPM2bxCGXaBuhs8__7I",
+  authDomain: "optixtoolkit.firebaseapp.com",
+  databaseURL: "https://optixtoolkit.firebaseio.com",
+  projectId: "optixtoolkit",
+  storageBucket: "optixtoolkit.appspot.com",
+  messagingSenderId: "227710522821",
+  appId: "1:227710522821:web:8992302e340bb9c1b767ac",
+  measurementId: "G-0M4XPGFFXM"
+})
+
+const email = 'SUM OF THAT THAT UNAME'
+const password = 'SUM OF THAT PASSWORD'
+
+async function main() {
+  const { user } = await firebase.auth().signInWithEmailAndPassword(email, password)
+  const id_token = await user.getIdToken()
+  const res = await fetch("http://localhost:3000/api/get", {
+    method: "post",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json",
+      "Accept-Charset": "utf-8"
+    },
+    body: JSON.stringify({
+      auth: id_token
+    })
+  })
+  console.log(await res.json())
+}
+
+main()
