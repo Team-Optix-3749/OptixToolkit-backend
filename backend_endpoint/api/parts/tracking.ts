2a3f5d50730cf4ca060de93b739109749dbfaaa0
rjawesome <aonedtop@gmail.com>
2020-09-28 15:43:17 -0700

    move more programs from folders to files

---
+++ backend_endpoint/api/parts/tracking.ts
@@ -0,0 +1,48 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import * as tracker from 'delivery-tracker'
+
+const fetch = require('node-fetch')
+
+module.exports = async (req: NowRequest, res: NowResponse) => {
+	if (req.body === undefined || req.body.trackingId === undefined) {
+		res.send(
+			JSON.stringify({
+				err: 'Invalid request: please specify trackingId!',
+			})
+		)
+		return
+	}
+
+	const fetcher = await fetch(
+		`https://shipit-api.herokuapp.com/api/guess/${req.body.trackingId}`
+	)
+
+	const courierName = await fetcher.json()
+
+	const { trackingId } = req.body
+	const courier = tracker.courier(
+		tracker.COURIER[`${courierName[0]}`.toUpperCase()].CODE
+	)
+
+	courier.trace(trackingId, (err, result) => {
+		if (!err) {
+			res.send(
+				JSON.stringify(
+					{
+						status: 'success',
+						data: result.status,
+					},
+					null,
+					2
+				)
+			)
+			return
+		}
+		res.send(
+			JSON.stringify({
+				status: 'fail',
+				data: err,
+			})
+		)
+	})
+}
---
+++ backend_endpoint/api/tools/add_tool.ts
@@ -0,0 +1,25 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import { tools } from '../utils/models'
+import authorize from '../utils/authorize'
+
+module.exports = async (req: NowRequest, res: NowResponse) => {
+	if (!await authorize(req.body.auth, { admin: true })) {
+    res.status(400).json({ err: 'Unauthorized request!' })
+    return
+  }
+
+	try {
+    if (req.body.name === undefined || req.body.count === undefined || typeof(req.body.name) !== "string" || typeof(req.body.count) !== "number") throw new Error('Bad params')
+    console.log("creating tool")
+    await tools.create({
+      name: req.body.name,
+      count: req.body.count
+    })
+    console.log("tool created")
+    res.status(200).json({ err: false })
+  }
+  catch (e) {
+    console.log(e)
+    res.status(400).json({ err: 'Bad Params!' })
+  }
+}
