bd7c85d824669d6833b6c3a49a8eb132dd796738
Aadit Gupta <spacegiant21@gmail.com>
2020-10-16 23:09:53 -0700

    new tracking api

---
+++ backend_endpoint/api/parts/tracking2.ts
@@ -0,0 +1,27 @@
+import { config } from 'dotenv'
+import * as Easypost from '@easypost/api'
+
+config()
+
+const api = new Easypost(process.env.TOKEN);
+
+
+module.exports = async (req: NowRequest, res: NowResponse) => {
+	if (req.body === undefined || req.body.trackingId === undefined || req.body.carrier === undefined) {
+		res.send(
+			JSON.stringify({
+				err: 'Invalid request: please specify trackingId!',
+			})
+		)
+		return
+	}
+  
+
+  const tracker = new api.Tracker({
+    tracking_code: req.body.trackingId,
+    carrier: req.body.carrier,
+  });
+
+  tracker.save().then(console.log);
+
+}
