facdb54868205ed6375bc3bbe5b2150770aa7126
rjawesome <aonedtop@gmail.com>
2020-12-23 17:47:59 -0800

    parts stuff more

--- backend_endpoint/api/parts/webhook/[secret].ts
+++ backend_endpoint/api/parts/webhook/[secret].ts
@@ -5,19 +5,22 @@ import { parts, Part } from '../../utils/models'
 module.exports = async (req: NowRequest, res: NowResponse) => {
   const { secret } = req.query
   if (secret !== WEBHOOK_SECRET || req.body.description !== "tracker.updated") {
+    console.log(secret)
     console.log(secret === WEBHOOK_SECRET)
     console.log("died at first part")
-    res.status(200)
+    res.status(200).json({aight: "cool"})
     return
   }
-  const { status, tracking_code, carrier } = req.body
+  const { status, tracking_code, carrier } = req.body.result
   if (typeof(status) !== "string" || typeof(tracking_code) !== "string" || typeof(carrier) !== "string") {
     console.log("died at second part")
-    res.status(200)
+    res.status(200).json({aight: "cool"})
     return
   }
 
-  await parts.updateMany(
+  console.log("starting that update")
+
+  const r = await parts.updateMany(
     {
       trackingInfo: { 
         trackingId: tracking_code, 
@@ -26,10 +29,12 @@ module.exports = async (req: NowRequest, res: NowResponse) => {
     }, 
     { 
       $set: { 
-        "trackingInfo.status": status
+        status: status
       } 
     }
   )
 
-  res.status(200)
+  console.log(r.n)
+
+  res.status(200).json({aight: "cool"})
 }
--- backend_endpoint/api/utils/config.ts
+++ backend_endpoint/api/utils/config.ts
@@ -2,4 +2,6 @@ import { config } from 'dotenv'
 
 config()
 
+console.log(process.env.MONGO_URL, process.env.WEBHOOK_SECRET)
+
 export const { MONGO_URL, WEBHOOK_SECRET } = process.env
