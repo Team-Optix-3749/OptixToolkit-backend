70e28f26cf7cc32d27505548fba17610e97421f8
Rohan Juneja <rohanj2006@gmail.com>
2021-11-22 19:27:52 -0800

    Update get_tools.ts

--- src/tools/get_tools.ts
+++ src/tools/get_tools.ts
@@ -6,14 +6,19 @@ export default async function get_tools(req: Request, res: Response) {
 	if (await authorize(req.body.auth)) {
 		let toolsRes = await tools.find()
 		let newToolsRes: Tool[] = []
-		let allpromises: Promise<string>[] = []
+		let allpromises: Promise<any>[] = []
 		let done = 0
 		toolsRes.forEach(async (tool) => {
-			let promises: Promise<string>[] = []
-			tool.reservations.forEach((uid) => promises.push(getDisplayName(uid)))
+			let promises: Promise<any>[] = []
+			tool.reservations.forEach((uid) => promises.push({uid: uid, dName: getDisplayName(uid)}))
 			allpromises.concat(promises)
-			(tool as any).reservations_uid = tool.reservations
-			tool.reservations = await Promise.all(promises)
+			const cool = await Promise.all(promises)
+			tool.reservations = []
+			(tool as any).reservations_uid = []
+			for (var i of cool) {
+				tool.reservations.push(i.dName)
+				(tool as any).reservations_uid.push(i.uid))
+			}
 			newToolsRes.push(tool)
 		})
 		await Promise.all(allpromises)
