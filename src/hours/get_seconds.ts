d2de88edf00b4d0ee880a4e9e796d3e77a28b0a5
Aadit Gupta <50186752+aaditgupta21@users.noreply.github.com>
2021-11-28 20:25:46 -0800

    Create get_seconds.ts

---
+++ src/hours/get_seconds.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+import { authorize } from '../utils/firebase'
+import { users } from '../utils/models'
+
+export default async function check_in(req: Request, res: Response) {
+  const user = await authorize(req.body.auth)
+
+  if (!user) {
+		res.status(400).json({ err: 'Unauthorized request!' })
+		return
+	}
+
+  const userDoc = await users.findOne({ uid: user.uid })
+  
+  try {
+    res.status(200).json({ seconds: userDoc.seconds, err: false })
+  } catch (e) {
+    res.status(400).json({ err: "Server Error" })
+  } 
+}
