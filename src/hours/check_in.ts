172feec5ac01a02399122bd44e05bd29729f74da
rjawesome <aonedtop@gmail.com>
2021-11-28 19:45:36 -0800

    check in/check out

--- src/hours/check_in.ts
+++ src/hours/check_in.ts
@@ -1,5 +1,28 @@
 import { Request, Response } from 'express'
+import { authorize } from '../utils/firebase'
+import { settings, users } from '../utils/models'
 
 export default async function check_in(req: Request, res: Response) {
+  const user = await authorize(req.body.auth)
 
+  if (!user) {
+		res.status(400).json({ err: 'Unauthorized request!' })
+		return
+	}
+
+  const setting = await settings.findOne({ key: "checkInPassword" })
+  if (req.body.password !== setting.value) {
+    res.status(400).json({ err: "Check In Password Wrong! "})
+    return
+  }
+
+  const userDoc = users.findOne({ uid: user.uid })
+
+  userDoc.lastCheckIn = Date.now()
+
+  try {
+    res.status(200).json({ err: false })
+  } catch (e) {
+    res.status(400).json({ err: "Server Error" })
+  }
 }
--- src/hours/check_out.ts
+++ src/hours/check_out.ts
@@ -1,5 +1,33 @@
 import { Request, Response } from 'express'
+import { settings, users } from '../utils/models'
+import { authorize } from '../utils/firebase'
 
 export default async function check_out(req: Request, res: Response) {
-  
+  const user = await authorize(req.body.auth)
+
+  if (!user) {
+		res.status(400).json({ err: 'Unauthorized request!' })
+		return
+	}
+
+  const setting = await settings.findOne({ key: "checkOutPassword" })
+  if (req.body.password !== setting.value) {
+    res.status(400).json({ err: "Check Out Password Wrong! "})
+    return
+  }
+
+  const userDoc = await user.findOne({ uid: user.uid })
+
+  if (userDoc.lastCheckIn === null) {
+    res.status(400).json({ err: "You haven't checked in" })
+    return
+  }
+
+  const timeElapsed = Date.now() - userDoc.lastCheckIn
+
+  userDoc.seconds += timeElapsed
+
+  await userDoc.save()
+
+  res.status(200).json({ err: false })
 }
