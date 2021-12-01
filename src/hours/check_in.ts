f2f0128773727ebf6b9e8b86c8290e3a27fae579
Aadit Gupta <spacegiant21@gmail.com>
2021-12-01 11:42:13 -0800

    format check in

--- src/hours/check_in.ts
+++ src/hours/check_in.ts
@@ -3,49 +3,57 @@ import { authorize } from '../utils/firebase'
 import { settings, users } from '../utils/models'
 
 export default async function check_in(req: Request, res: Response) {
-  const user = await authorize(req.body.auth)
-  const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-  const weekdays = ['Tuesday','Thursday'];
+	const user = await authorize(req.body.auth)
+	const days = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday',
+	]
+	const weekdays = ['Tuesday', 'Thursday']
 
-  if (!user) {
+	if (!user) {
 		res.status(400).json({ err: 'Unauthorized request!' })
 		return
 	}
-   
-  const userDoc = await users.findOne({ uid: user.uid })
 
-  var date = new Date(Date.now()*1000 - 1000*8*3600);
-  
-  if (weekdays.includes(days[date.getDay()])) {
-    if (date.getHours() >= 15 && date.getHours() <= 18) {
-      userDoc.lastCheckIn = Date.now()
-    } else {
-      res.status(400).json({ err: "Not in meeting time!"})
-      return
-    }
-  } else if (days[date.getDay()] == "Saturday") {
-    if (date.getHours() >= 8 && date.getHours() <= 17) {
-      userDoc.lastCheckIn = Date.now()
-    } else {
-      res.status(400).json({ err: "Not in meeting time!"})
-      return
-    }
-  } else {
-    res.status(400).json({ err: "No meeting today!"})
-    return
-  }
-  
-  const setting = await settings.findOne({ key: "checkInPassword" })
-  if (req.body.password !== setting.value) {
-    res.status(400).json({ err: "Check In Password Wrong! "})
-    return
-  }
+	const userDoc = await users.findOne({ uid: user.uid })
 
-  await userDoc.save()
+	var date = new Date(Date.now() * 1000 - 1000 * 8 * 3600)
 
-  try {
-    res.status(200).json({ err: false })
-  } catch (e) {
-    res.status(400).json({ err: "Server Error" })
-  }
+	if (weekdays.includes(days[date.getDay()])) {
+		if (date.getHours() >= 15 && date.getHours() <= 18) {
+			userDoc.lastCheckIn = Date.now()
+		} else {
+			res.status(400).json({ err: 'Not in meeting time!' })
+			return
+		}
+	} else if (days[date.getDay()] == 'Saturday') {
+		if (date.getHours() >= 8 && date.getHours() <= 17) {
+			userDoc.lastCheckIn = Date.now()
+		} else {
+			res.status(400).json({ err: 'Not in meeting time!' })
+			return
+		}
+	} else {
+		res.status(400).json({ err: 'No meeting today!' })
+		return
+	}
+
+	const setting = await settings.findOne({ key: 'checkInPassword' })
+	if (req.body.password !== setting.value) {
+		res.status(400).json({ err: 'Check In Password Wrong! ' })
+		return
+	}
+
+	await userDoc.save()
+
+	try {
+		res.status(200).json({ err: false })
+	} catch (e) {
+		res.status(400).json({ err: 'Server Error' })
+	}
 }
