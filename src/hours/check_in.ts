87ea53a641adc4c53f2a0dc320261e97a0554200
Raadwan Masum <piraadwan@gmail.com>
2021-12-01 10:14:23 -0800

    add timing functionality for checkin

--- src/hours/check_in.ts
+++ src/hours/check_in.ts
@@ -4,22 +4,43 @@ import { settings, users } from '../utils/models'
 
 export default async function check_in(req: Request, res: Response) {
   const user = await authorize(req.body.auth)
+  const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  const weekdays = ['Tuesday','Thursday'];
 
   if (!user) {
 		res.status(400).json({ err: 'Unauthorized request!' })
 		return
 	}
+   
+  const userDoc = await users.findOne({ uid: user.uid })
 
+  var date = new Date(Date.now()*1000 - 1000*8*3600);
+  
+  if (weekdays.includes(days[date.getDay()])) {
+    if (date.getHours() >= 15 && date.getHours() <= 18) {
+      userDoc.lastCheckIn = Date.now()
+    } else {
+      res.status(400).json({ err: "Not in meeting time!"})
+      return
+    }
+  } else if (days[date.getDay()] == "Saturday") {
+    if (date.getHours() >= 8 && date.getHours() <= 17) {
+      userDoc.lastCheckIn = Date.now()
+    } else {
+      res.status(400).json({ err: "Not in meeting time!"})
+      return
+    }
+  } else {
+    res.status(400).json({ err: "No meeting today!"})
+    return
+  }
+  
   const setting = await settings.findOne({ key: "checkInPassword" })
   if (req.body.password !== setting.value) {
     res.status(400).json({ err: "Check In Password Wrong! "})
     return
   }
 
-  const userDoc = await users.findOne({ uid: user.uid })
-
-  userDoc.lastCheckIn = Date.now()
-	
   await userDoc.save()
 
   try {
