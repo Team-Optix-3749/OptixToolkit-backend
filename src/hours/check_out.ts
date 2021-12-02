d3f8659d8c86e9214c78ee276e53823230aa94af
Raadwan Masum <piraadwan@gmail.com>
2021-12-01 22:51:43 -0800

    attempt to fix set lastCheckIn to null

--- src/hours/check_out.ts
+++ src/hours/check_out.ts
@@ -43,7 +43,7 @@ export default async function check_out(req: Request, res: Response) {
 	} else if (weekdays.includes(days[date.getDay()])) {
 		if (date.getHours() >= 15 && date.getHours() <= 18) {
 			userDoc.seconds = Date.now() - userDoc.lastCheckIn
-			userDoc.lastCheckIn = null
+			userDoc.lastCheckIn = ""
 			userDoc.meetingCount++
 		} else {
 			res.status(400).json({ err: 'Not in meeting time!' })
@@ -52,7 +52,7 @@ export default async function check_out(req: Request, res: Response) {
 	} else if (days[date.getDay()] === 'Saturday') {
 		if (date.getHours() >= 8 && date.getHours() <= 17) {
 			userDoc.seconds = Date.now() - userDoc.lastCheckIn
-			userDoc.lastCheckIn = null
+			userDoc.lastCheckIn = ""
 			userDoc.meetingCount++
 		} else {
 			res.status(400).json({ err: 'Not in meeting time!' })
