e73f5aabd0d10787ca7a3dfca34ebe43fe90fdf5
Raadwan Masum <piraadwan@gmail.com>
2021-12-01 21:24:21 -0800

    implement meeting override
    Co-authored-by: Aadit Gupta <aaditgupta21@users.noreply.github.com>

--- src/hours/check_in.ts
+++ src/hours/check_in.ts
@@ -20,18 +20,25 @@ export default async function check_in(req: Request, res: Response) {
 		return
 	}
 
+	const setting = await settings.findOne({ key: 'checkInPassword' })
+	if (req.body.password !== setting.value) {
+		res.status(400).json({ err: 'Check In Password Wrong! ' })
+		return
+	}
+
 	const userDoc = await users.findOne({ uid: user.uid })
+	const attendanceOverride = await settings.findOne({ key: 'attendanceOverride' })
 
 	var date = new Date(Date.now() * 1000 - 1000 * 8 * 3600)
 
-	if (weekdays.includes(days[date.getDay()])) {
+	if (attendanceOverride || weekdays.includes(days[date.getDay()])) {
 		if (date.getHours() >= 15 && date.getHours() <= 18) {
 			userDoc.lastCheckIn = Date.now()
 		} else {
 			res.status(400).json({ err: 'Not in meeting time!' })
 			return
 		}
-	} else if (days[date.getDay()] == 'Saturday') {
+	} else if (attendanceOverride || days[date.getDay()] == 'Saturday') {
 		if (date.getHours() >= 8 && date.getHours() <= 17) {
 			userDoc.lastCheckIn = Date.now()
 		} else {
@@ -40,12 +47,6 @@ export default async function check_in(req: Request, res: Response) {
 		}
 	} else {
 		res.status(400).json({ err: 'No meeting today!' })
-		return
-	}
-
-	const setting = await settings.findOne({ key: 'checkInPassword' })
-	if (req.body.password !== setting.value) {
-		res.status(400).json({ err: 'Check In Password Wrong! ' })
 		return
 	}
 
