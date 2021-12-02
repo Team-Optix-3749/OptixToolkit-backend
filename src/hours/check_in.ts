7cc2ffa9b891260ee981e1d3e55f86e49a977a11
Raadwan Masum <piraadwan@gmail.com>
2021-12-02 14:38:53 -0800

    fix dates

--- src/hours/check_in.ts
+++ src/hours/check_in.ts
@@ -38,15 +38,15 @@ export default async function check_in(req: Request, res: Response) {
 		return
 	} else if (attendanceOverride === "true") {
 		userDoc.lastCheckIn = Date.now()
-	} else if (weekdays.includes(days[date.getDay()])) {
-		if (date.getHours() >= 15 && date.getHours() <= 18) {
+	} else if (date.getDay() === 2 || date.getDay() === 5) {
+		if (date.getHours() >= 14 && date.getHours() <= 17) {
 			userDoc.lastCheckIn = Date.now()
 		} else {
 			res.status(400).json({ err: 'Not in meeting time!' })
 			return
 		}
-	} else if (days[date.getDay()] === 'Saturday') {
-		if (date.getHours() >= 8 && date.getHours() <= 17) {
+	} else if (date.getDay() === 6) {
+		if (date.getHours() >= 7 && date.getHours() <= 16) {
 			userDoc.lastCheckIn = Date.now()
 		} else {
 			res.status(400).json({ err: 'Not in meeting time!' })
