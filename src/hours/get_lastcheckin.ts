fab21468beee5fb34eb846e07306c1654335f555
Raadwan Masum <piraadwan@gmail.com>
2021-12-02 20:46:26 -0800

    create endpoint to get last check in

---
+++ src/hours/get_lastcheckin.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express'
+import { authorize } from '../utils/firebase'
+import { users } from '../utils/models'
+
+export default async function get_meetings(req: Request, res: Response) {
+  const user = await authorize(req.body.auth)
+
+  if (!user) {
+		res.status(400).json({ err: 'Unauthorized request!' })
+		return
+	}
+
+  try {
+	 const userDoc = await users.findOne({ uid: user.uid })
+    res.status(200).json({ count: userDoc.lastCheckIn, err: false })
+  } catch (e) {
+    res.status(400).json({ err: "Server Error" })
+  } 
+}
