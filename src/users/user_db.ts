d9d34b55c092ea24c2449d2c9889781ff33643ee
Aadit Gupta <50186752+aaditgupta21@users.noreply.github.com>
2021-12-05 14:39:06 -0800

    Create user_db.ts

---
+++ src/users/user_db.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express'
+import { users, User } from '../utils/models'
+import { authorize } from '../utils/firebase'
+
+
+export default async function user_db(req: Request, res: Response) {
+	if (!(await authorize(req.body.auth, { type: 'admin' }))) {
+		res.status(400).json({ err: 'Unauthorized request!' })
+		return
+	}
+
+	try {
+		await users.create({
+			uid: req.body.uid,
+			lastCheckIn: 0,
+			seconds: 0,
+			meetingCount: 0,
+		})
+		res.status(200).json({ err: false })
+	} catch (e) {
+		console.log(e)
+		res.status(400).json({ err: 'Bad Params!' })
+	}
+}
