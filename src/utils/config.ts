90f7f2b9486db4e93853f1cda1b4b07935f23398
Aadit Gupta <50186752+aaditgupta21@users.noreply.github.com>
2021-12-09 16:30:19 -0800

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,5 +7,13 @@ if (
 
 console.log(process.env.MONGO_URL, process.env.WEBHOOK_SECRET)
 
-export const { MONGO_URL, WEBHOOK_SECRET, SENDGRID_KEY, FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } = process.env
+export const {
+	USER_SECRET,
+	MONGO_URL,
+	WEBHOOK_SECRET,
+	SENDGRID_KEY,
+	FIREBASE_PROJECT_ID,
+	FIREBASE_CLIENT_EMAIL,
+	FIREBASE_PRIVATE_KEY,
+} = process.env
 export const PORT = process.env.PORT ?? 4000
