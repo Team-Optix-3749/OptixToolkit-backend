1f572aaf06bab7b8f32e7fcffb3d2e1ff95da203
SlushEE0 <neeladem2009@gmail.com>
2023-12-14 20:27:21 -0800

    changed firebase project

--- src/frontend/admin_panel/src/lib/secrets.ts
+++ src/frontend/admin_panel/src/lib/secrets.ts
@@ -1,7 +1,5 @@
-const VITE_ENV = import.meta.env;
-
 const SECRETS = {
-  BACKEND_URL: VITE_ENV.VITE_BACKEND_URL,
+  BACKEND_URL: window.location.host,
   TABLE_DEFS: [
     { field: "userName", headerName: "Name" },
     { field: "hours", headerName: "Participation Hours" },
@@ -9,7 +7,17 @@ const SECRETS = {
     { field: "seconds", headerName: "Participation Seconds" },
     { field: "lastCheckIn" },
     { field: "uid", headerName: "UserID" }
-  ]
+  ],
+  FIREBASECFG: {
+    apiKey: "AIzaSyBywkBF8HlaLDTgvPM2bxCGXaBuhs8__7I",
+    authDomain: "optixtoolkit.firebaseapp.com",
+    databaseURL: "https://optixtoolkit.firebaseio.com/",
+    projectId: "optixtoolkit",
+    storageBucket: "optixtoolkit.appspot.com",
+    messagingSenderId: "227710522821",
+    appId: "1:227710522821:web:8992302e340bb9c1b767ac",
+    measurementId: "G-0M4XPGFFXM"
+  }
 };
 
 export default SECRETS;
--- src/frontend/admin_panel/src/lib/utils/db/firebase.ts
+++ src/frontend/admin_panel/src/lib/utils/db/firebase.ts
@@ -1,47 +1,7 @@
 import { FirebaseApp, initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDI04e7u2jaeyjvjZTZCyits0KeMI6KKIk",
-  authDomain: "garageopener-27000.firebaseapp.com",
-  databaseURL: "https://garageopener-27000-default-rtdb.firebaseio.com",
-  projectId: "garageopener-27000",
-  storageBucket: "garageopener-27000.appspot.com",
-  messagingSenderId: "1066651715164",
-  appId: "1:1066651715164:web:b16a994346ad5fc20e8899",
-  measurementId: "G-YSQX2FQCZX"
-};
+import SECRETS from "../../secrets";
 
-//house -> users
-//settings & keys -> settings
-//new one for parts barcodse
+const firebaseConfig = SECRETS.FIREBASECFG;
 
 export const firebaseApp: FirebaseApp = initializeApp(firebaseConfig);
-
-// const firestore = getFirestore(firebaseApp);
-
-// export async function getUserData(userID: string) {
-//   const login = Realm.Credentials.anonymous();
-//   realmApp.logIn(login);
-
-//   if (!mongo) throw new Error("Mongo DB current user undefined");
-//   const users_collection = mongo?.db("ToolkitData").collection("userData");
-
-//   return await users_collection.findOne({ uid: userID });
-// }
-
-// let adminUsers: string[];
-// export async function getAdminUsers() {
-//   //write code to get valid users from firebase
-//   if (adminUsers) return adminUsers;
-
-//   const [value, , error] = useDocumentData(doc(firestore, "users/admins"));
-
-//   if (error) throw new Error(error as unknown as string);
-
-//   console.log(value);
-
-//   const fetchedValidUsers: string[] = [];
-//   adminUsers = fetchedValidUsers;
-
-//   return adminUsers;
-// }
