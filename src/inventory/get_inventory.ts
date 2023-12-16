c36e80c49a6c7d30aa0421391e94c90f723c75ac
Rohan Juneja <rohanj2006@gmail.com>
2023-12-16 15:50:57 -0800

    syntax fix

--- src/inventory/get_inventory.ts
+++ src/inventory/get_inventory.ts
@@ -8,10 +8,7 @@ export default async function get_inventory(req: Request, res: Response) {
 			res.status(400).json({ err: 'Bad Params!' })
 			return
 		}
-		console.log(req.body.barcodeId)
-		const inventoryRes = await inventory.findOne({ barcodeId: req.body.barcodeId })
-				console.log(inventoryRes)
-		console.log(inventory.collection.collectionName)
+		const inventoryRes = await inventory.findOne({ barcodeId: req.body.barcodeId }).exec()
 		res.status(200).json({ inventory: inventoryRes, err: false })
 	} else {
 		res.status(400).json({ err: 'Unauthorized request!' })
