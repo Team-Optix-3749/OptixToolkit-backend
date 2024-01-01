bb53daf998ede143fbc7398a6a5cb266749500cc
Rohan Juneja <rohanj2006@gmail.com>
2024-01-01 13:56:31 -0800

    return new inventory

--- src/inventory/modify_inventory.ts
+++ src/inventory/modify_inventory.ts
@@ -10,13 +10,12 @@ export default async function modify_inventory(req: Request, res: Response) {
 		if (inventoryTool) {
 			if (req.body.status) inventoryTool.status = req.body.status;
 			if (req.body.location) inventoryTool.location = req.body.location;
-			await inventoryTool.save();
+			const newInventory = await inventoryTool.save();
+			res.status(200).json({ err: false, inventory: newInventory })
 		}
       else {
          res.status(400).json({ err: 'This inventoried tool does not exist' })
       }
-
-		res.status(200).json({ err: false })
 	} else {
 		res.status(400).json({ err: 'Unauthorized request or no changes made!' })
 	}
