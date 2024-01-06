208dfd2b0015ab54dd21781063b6520dcf0e812f
Rohan Juneja <rohanj2006@gmail.com>
2024-01-06 13:00:40 -0800

    Update get_lastcheckin.ts pls compile

--- src/hours/get_lastcheckin.ts
+++ src/hours/get_lastcheckin.ts
@@ -11,7 +11,18 @@ export default async function get_lastcheckin(req: Request, res: Response) {
 	}
 
   try {
-	 const userDoc = await users.findOne({ uid: user.uid })
+	 let userDoc = await users.findOne({ uid: user.uid })
+
+	  if (userDoc == null) {
+		await users.create({
+				uid: user.uid,
+				lastCheckIn: 0,
+				seconds: 0,
+				meetingCount: 0,
+			})
+		userDoc = await users.findOne({ uid: user.uid })
+	}
+	  
     res.status(200).json({ lastcheckin: userDoc.lastCheckIn, err: false })
   } catch (e) {
     res.status(400).json({ err: "Server Error" })
