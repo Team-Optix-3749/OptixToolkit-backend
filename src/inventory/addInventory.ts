60ad8662e827ecdc1307345bd2675d1f513fd208
PaarasPurohit <pmpurohit2017@outlook.com>
2024-12-16 10:50:35 -0800

    remove admin perms on adding inventory

--- src/inventory/addInventory.ts
+++ src/inventory/addInventory.ts
@@ -3,22 +3,18 @@ import { inventory, Inventory } from '../utils/models'
 import { authorize } from '../utils/firebase'
 
 function validateInventory(body: any): body is Inventory {
-	return typeof body.name === 'string' && typeof body.barcodeId === 'string' && typeof body.count === 'number'
+	return typeof body.name === 'string' && typeof body.barcodeId === 'string' && typeof body.category === 'string' && typeof body.count === 'number'
 }
 
 export default async function addInventory(req: Request, res: Response) {
-	if (!(await authorize(req.body.auth, { type: 'admin' }))) {
-		res.status(400).json({ err: 'Unauthorized request!' })
-		return
-	}
 
 	try {
 		if (!validateInventory(req.body)) throw new Error('Bad params')
 		await inventory.create({
 			name: req.body.name,
+			barcodeId: req.body.barcodeId,
+			category: req.body.category,
 			count: req.body.count,
-            barcodeId: req.body.barcodeId,
-            category: req.body.category,
 		})
 		res.status(200).json({ err: false })
 	} catch (e) {
